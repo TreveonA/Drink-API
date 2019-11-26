dfdd78bb394b530fc098dbc03e837d12de8843b3
TreveonA <treveon.alexander@gmail.com>
2019-11-25 21:27:06 -0500

    Quick Cocktail2

--- index.js
+++ index.js
@@ -10,18 +10,34 @@ function drinkRecipe(drinks) {
 
 function displayRecipe(recipe) {
     let recipeCard = document.querySelector('body')
-    console.log(recipe)
+
+    console.log(recipe['strIngredient2'])
     let container = document.createElement('div')
     let cName = document.createElement('h2')
     let iTitle = document.createElement('h3')
     let instructionsTitle = document.createElement('h3')
     let image = document.createElement('img')
     let ingList = document.createElement('ul')
+    for (let i = 1; i<= 15; i++) {
+        if (recipe['strIngredient'+i]!=null){
+            let li = document.createElement('li')
+            li.innerText = recipe['strIngredient'+i]
+            ingList.appendChild(li)
+        }
+    }
     let ins = document.createElement('p')
     cName.innerText = recipe.strDrink
+    iTitle.innerText = "Ingredients"
+    instructionsTitle.innerText = "Instuctions"
+    ins.innerText = recipe.strInstructions
+    image.src = recipe.strDrinkThumb 
     container.appendChild(cName)
+    container.appendChild(image)
+    container.appendChild(iTitle)
+    container.appendChild(ingList)
+    container.appendChild(instructionsTitle)
+    container.appendChild(ins)
     recipeCard.appendChild(container)
-    recipeCard.appendChild(iTitle)
 }
 
 document.getElementById("drinkPicker").addEventListener('submit',drinker)
